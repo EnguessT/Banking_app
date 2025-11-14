e288050b6811998d72ee30cb6929f1678dc0831c
Thierry <enguessthierry@gmail.com>
2025-11-14 16:44:49 +0100

    created the main window GUI window

--- src/main.c
+++ src/main.c
@@ -1,9 +1,15 @@
 #include <stdio.h>
+#include <gtk/gtk.h>
+#include "graphical_widgets.h"
 
 int main(int argc, char* argv[]) {
 
-	(void)argc;
-	(void)argv;
+	gtk_init(&argc, &argv);
+
+	GtkWidget *window = create_main_window();
+    gtk_widget_show_all(window);
+
+    gtk_main();
 
 	return 0;
 }
